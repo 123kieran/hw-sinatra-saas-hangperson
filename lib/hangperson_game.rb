ef33f20f70d560071ed0de969119ba1f53111fb6
EC2 Default User <ec2-user@ip-172-31-0-51.us-east-2.compute.internal>
2019-02-08 17:43:11 +0000

    first 12 examples passing

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -13,6 +13,25 @@ class HangpersonGame
     @guesses = ''
     @wrong_guesses = ''
   end
+  
+   def guess guess
+    
+    # if guess is contained in the alphabet
+    if guess =~ /[[:alpha:]]/
+      guess.downcase!
+      if @word.include? guess and !@guesses.include? guess
+        @guesses.concat guess
+      elsif !@wrong_guesses.include? guess and !@word.include? guess
+        @wrong_guesses.concat guess
+        return true
+      else
+        return false
+      end
+    else
+      guess = :invalid
+      raise ArgumentError
+    end
+  end
 
   # You can test it by running $ bundle exec irb -I. -r app.rb
   # And then in the irb: irb(main):001:0> HangpersonGame.get_random_word
